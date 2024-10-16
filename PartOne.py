ee556686a2896c62394e37479f7bd16b7247c190
Rithi-Laksun <rl471@kent.ac.uk>
2024-10-16 10:25:01 +0000

    converting camel to snake case

--- PartOne.py
+++ PartOne.py
@@ -1,1 +1,8 @@
+# converting camel case input into snake case output
 
+def main(name):
+
+    return ''.join(['_'+i.lower() if i.isupper() 
+                    else i for i in name]).lstrip('_')
+name = input("What's your full name?")
+print(name)
